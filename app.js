13f231e5b7b78298e4994d9dbeedb3e70b86be4b
Valentina <vjromero@fi.uba.ar>
2024-11-19 17:30:04 -0300

    Get user/s

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-let users = []
-let princesas = []
-let principes = []
-let villanos = []
+let users = [{
+  id: 1,
+  name: "Maria",
+  age: 12,
+  fav_princess: "Bella Durmiente",
+}, {
+  id: 2,
+  name: "Bruno",
+  age: 17,
+  fav_princess: "La Sirenita"
+}]
+let princesses = []
+let princes = []
+let villains = []
 let favorite_princess = []
 let princess_villain = []
 let the_prince_of_the_princess = []
@@ -15,6 +25,19 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/v1/users', (req, res) => {
+  res.json(users)
+})
+
+app.get('/api/v1/users/:id', (req, res) => {
+  const user = users.find((element) => element.id == req.params.id)
+
+  if (user === undefined) {
+    res.sendStatus(404)
+  }
+  res.json(user)
 })
 
 app.listen(port, () => {
