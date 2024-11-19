b77e13b331fdc8325d1a60d7a5b03163318b774e
Rochi <rvera@fi.uba.ar>
2024-11-19 15:13:08 -0300

    Express skeleton
    Express skeleton, armo las bases para empezar a realizar las tablas

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let users = []
+let princesas = []
+let principes = []
+let villanos = []
+let favorite_princess = []
+let princess_villain = []
+let the_prince_of_the_princess = []
+
+
 app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
