55a0192494fd459e75911e33d6e6d4689ff3b874
Rocio-Elizabeth-Vera <rvera@fi.uba.ar>
2024-11-24 19:07:57 -0300

    Agrego ruta de la api principes y arreglo lo que me falto en la ruta de princesas

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const app = express();
 const puerto = 3000;
 const prisma = new PrismaClient();
 const usuarios = require('./routes/usuarios');
+const princesas = require('./routes/princesas');
+const principes = require('./routes/principes');
 
 app.use(express.json());
 app.use(cors());
@@ -15,6 +17,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/usuarios', usuarios);
 app.use('/api/v1/princesas', princesas);
+app.use('/api/v1/principes',principes);
+
 app.listen(puerto, () => {
   console.log(`Princesas escuchando en el puerto ${puerto}`);
 });
