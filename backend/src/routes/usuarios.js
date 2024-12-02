34447fcdf6a1d677d483ccc4036166aba50ca5b4
oswald <omaldonado@fi.uba.ar>
2024-12-02 18:15:01 -0300

    cambios en las tabla relacionales
    estoy intentando hacer el crud para agregarle a un usuario especifico un personaje

--- backend/src/routes/usuarios.js
+++ backend/src/routes/usuarios.js
@@ -23,6 +23,25 @@ router.get('/:id', async (req, res) => {
   res.json(usuario)
 })  
   
+router.get('/:id/personajes', async (req, res) => {
+  const usuario = await prisma.usuario.findUnique({ 
+    where: {
+      id : parseInt(req.params.id)
+    },
+    include: {
+      usuarios_princesas: true,
+      usuarios_principes: true,
+      usuarios_villanos: true
+  },
+  })
+  
+  if (usuario === null) {
+    res.sendStatus(404)
+    return
+  }
+  res.json(usuario)
+})
+
 router.post('/', async (req, res) => {
   const usuario = await prisma.usuario.create({
     data:{
@@ -35,6 +54,35 @@ router.post('/', async (req, res) => {
   })
   res.status(201).send(usuario)
 })
+
+router.post('/:id/princesas/:princesa_id', async (req, res) => {
+  const usuario = await prisma.usuario.findUnique({
+    where: {
+      id: parseInt(req.params.id)
+    }
+  });
+  if (usuario === null) {
+    res.sendStatus(404);
+    return;
+  }
+  const princesa = await prisma.princesa.findUnique({
+    where: {
+      id: parseInt(req.params.princesa_id)
+    }
+  });
+  if (princesa === null) {
+    res.sendStatus(404);
+    return;
+  }
+  const usuarioPrincesa = await prisma.usuarios_princesas.create({
+    data: {
+      usuario_id: parseInt(req.params.id),
+      princesa_id: parseInt(req.params.princesa_id)
+    }
+  });
+  res.status(201).send(usuarioPrincesa);
+  console.log(usuarioPrincesa);
+});
   
 router.delete('/:id', async (req, res) => {
   const usuario = await prisma.usuario.findUnique({
